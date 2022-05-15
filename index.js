d211be1301145e3ba51988e7f49c27537f338b52
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-05-15 15:53:12 +0530

    add database - get and post

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -19,31 +21,11 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/info", (request, response) => {
-  const totalPersons = persons.length;
+  let totalPersons;
+  Person.find({}).then((persons) => {
+    totalPersons = persons.length;
+  });
   response.send(`The phonebook has ${totalPersons} people \n
    ${new Date()}`);
   console.log(
@@ -52,18 +34,16 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+    ``;
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -80,21 +60,15 @@ app.post("/api/persons", (request, response) => {
       error: "The name or number is missing",
     });
   }
-  if (persons.find((p) => p.name === body.name)) {
-    return response.status(400).json({
-      error: "The name already exists",
-    });
-  }
-  const person = {
+
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: getRandomInt(100),
-  };
+  });
 
-  persons = persons.concat(person);
-  // console.log(persons);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
