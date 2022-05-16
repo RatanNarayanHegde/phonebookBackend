d5794de0cfc6ebbe494af631d691a2b5168d955b
RatanNarayanHegde <ratanhegde08@gmail.com>
2022-05-16 21:42:18 +0530

    add build files

--- index.js
+++ index.js
@@ -59,14 +59,17 @@ app.put("/api/persons/:id", (request, response, next) => {
     number: request.body.number,
   };
 
-  Person.findByIdAndUpdate(request.body.id, person, { new: true })
+  Person.findByIdAndUpdate(request.body.id, person, {
+    new: true,
+    runValidators: true,
+  })
     .then((returnedPerson) => {
       response.json(returnedPerson);
     })
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -79,9 +82,14 @@ app.post("/api/persons", (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 const errorHandler = (error, request, response, next) => {
@@ -89,6 +97,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).send({ error: error.message });
   }
 
   next(error);
